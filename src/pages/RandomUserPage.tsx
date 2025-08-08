d51580b9cca3a5378647e81fe7b6d3c59ca07826
Kisaru1523 <76036259+Kisaru1523@users.noreply.github.com>
2025-08-08 17:38:16 +0700

    Finish

--- src/pages/RandomUserPage.tsx
+++ src/pages/RandomUserPage.tsx
@@ -1,9 +1,10 @@
 import { UserCard } from "../components/UserCard";
 import { cleanUser } from "../libs/CleanUser";
 import axios from "axios";
+import { useEffect } from "react";
 import { useState } from "react";
 export default function RandomUserPage() {
-  const [users, setUsers] = useState("");
+  const [users, setUsers] = useState<any[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
 
@@ -14,10 +15,20 @@ export default function RandomUserPage() {
     );
     setIsLoading(false);
     const users = resp.data.results;
-    //Your code here
+    const cleanUsers = users.map((user: any) => cleanUser(user)); //Clean the user data
+    setUsers(cleanUsers); // Update the state with cleaned user data
+    localStorage.setItem("users", JSON.stringify(cleanUsers));
+     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/CleanUser
     //Then update state with function : setUsers(...)
   };
+
+  useEffect(() => {
+    const savedAmount = localStorage.getItem("genAmount");
+    if (savedAmount) {
+      setGenAmount(Number(savedAmount));
+    }
+  }, []);
 
   return (
     <div style={{ maxWidth: "700px" }} className="mx-auto">
@@ -28,17 +39,36 @@ export default function RandomUserPage() {
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
-          onChange={(event: any) => setGenAmount(event.target.value)}
           value={genAmount}
+          onChange={(event: any) => {
+            const value = Number(event.target.value);
+            setGenAmount(value);
+            localStorage.setItem("genAmount", String(value));
+          }}
         />
         <button className="btn btn-dark" onClick={generateBtnOnClick}>
           Generate
         </button>
       </div>
+
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users.length > 0 &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            key={user.email} // ใช้ email เป็น key ตามคำแนะนำ
+            name={user.name}
+            email={user.email}
+            address={user.address}
+            imgUrl={user.imgUrl}
+          />
+        ))}
+
+      {users.length === 0 && !isLoading && (
+        <p className="display-6 text-center fst-italic my-4">No User</p>
+      )}
     </div>
   );
 }
